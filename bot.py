ceca16b6b6ccf0fd24414ab34e6f373757b70655
梓澪 <zilingmio@gmail.com>
2026-03-22 02:31:12 +0800

    ✨ feat(bot): 初始化自定义菜单设置逻辑，移除冗余命令

--- bot.py
+++ bot.py
@@ -2,10 +2,11 @@ import asyncio
 
 from pyrogram import Client
 from pyrogram.handlers import ConnectHandler, DisconnectHandler
+from pyrogram.types import BotCommand
 
 from core.config import bs, ws
 from core.watchdog import on_connect, on_disconnect
-from log import setup_logging
+from log import logger, setup_logging
 from utils.event_loop import setup_optimized_event_loop
 
 setup_logging(debug=bs.debug)
@@ -30,6 +31,7 @@ class Bot(Client):
     async def start(self, **kwargs):
         self.init_watchdog()
         await super().start()
+        await self.set_menu()
 
     async def stop(self, *args):
         ws.exit_flag = True
@@ -38,6 +40,14 @@ class Bot(Client):
     def init_watchdog(self):
         self.add_handler(ConnectHandler(on_connect))
         self.add_handler(DisconnectHandler(on_disconnect))
+
+    async def set_menu(self):
+        commands = {
+            "start": "开始",
+            "help": "帮助",
+        }
+        await self.set_bot_commands([BotCommand(command=k, description=v) for k, v in commands.items()])
+        logger.debug(f"菜单已设置: {commands}")
 
 
 if __name__ == "__main__":
--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import BotCommand, Message
-
-from utils.filters import is_admin
+from pyrogram.types import Message
 
 
 @Client.on_message(filters.command(["start", "help"]))
@@ -12,13 +10,3 @@ async def start(_, msg: Message):
 @Client.on_message(filters.command("ping"))
 async def ping(_, msg: Message):
     await msg.reply_text("Pong!")
-
-
-@Client.on_message(filters.command("menu") & is_admin)
-async def set_menu(cli: Client, msg: Message):
-    commands = {
-        "start": "开始",
-        "help": "帮助",
-    }
-    await cli.set_bot_commands([BotCommand(command=k, description=v) for k, v in commands.items()])
-    await msg.reply("👌")
